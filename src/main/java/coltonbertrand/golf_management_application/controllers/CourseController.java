c369b0182b4baa3d1cd8f29e199dfc5f1724a41c
colton <coltonbertrand14@gmail.com>
2024-12-20 20:44:07 -0400

    Worked controller classes

--- src/main/java/coltonbertrand/golf_management_application/controllers/CourseController.java
+++ src/main/java/coltonbertrand/golf_management_application/controllers/CourseController.java
@@ -1,4 +1,25 @@
 package coltonbertrand.golf_management_application.controllers;
 
+import coltonbertrand.golf_management_application.classes.Courses;
+import coltonbertrand.golf_management_application.classes.Users;
+import coltonbertrand.golf_management_application.services.CoursesService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/courses")
 public class CourseController {
+
+    @Autowired
+    CoursesService coursesService;
+    @PostMapping("/saveCourse")
+    public Courses addCourse(@RequestBody Courses course, HttpSession session){
+        Users user = (Users) session.getAttribute("user");
+        return coursesService.addCourse(course,user.getId());
+    }
 }
--- src/main/java/coltonbertrand/golf_management_application/controllers/UserController.java
+++ src/main/java/coltonbertrand/golf_management_application/controllers/UserController.java
@@ -1,4 +1,29 @@
 package coltonbertrand.golf_management_application.controllers;
 
+import coltonbertrand.golf_management_application.classes.Users;
+import coltonbertrand.golf_management_application.services.UsersService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    UsersService usersService;
+
+    @PostMapping("/register")
+    public Users registerUser(@Valid @RequestBody Users user) {
+        return usersService.addUser(user);
+    }
+
+    @GetMapping("/login")
+    public Users login(@RequestBody Users user, HttpSession session){
+           Users authUser = usersService.loginUser(user.getEmail(),user.getPassword());
+           session.setAttribute("user",authUser);
+           return authUser;
+    }
 }
