409ec27e316fd68d62401a83139290ddad64c5d3
colton <coltonbertrand14@gmail.com>
2025-01-06 18:50:39 -0400

    Changed get course holes function

--- src/main/java/coltonbertrand/golf_management_application/controllers/RoundHolesController.java
+++ src/main/java/coltonbertrand/golf_management_application/controllers/RoundHolesController.java
@@ -2,7 +2,9 @@ package coltonbertrand.golf_management_application.controllers;
 
 
 import coltonbertrand.golf_management_application.classes.Round_Holes;
+import coltonbertrand.golf_management_application.classes.Users;
 import coltonbertrand.golf_management_application.services.RoundHolesService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -20,8 +22,12 @@ public class RoundHolesController {
     @Autowired
     private RoundHolesService roundHolesService;
     @GetMapping("/get/{roundId}")
-    public ResponseEntity<List<Round_Holes>> getRoundHoles(Integer roundId){
+    public ResponseEntity<List<Round_Holes>> getRoundHoles(Integer roundId, HttpSession session){
+        Users user = (Users) session.getAttribute("user");
         List<Round_Holes> getHoles = roundHolesService.getHoles(roundId);
+        if(user == null){
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok().body(getHoles);
     }
 }
