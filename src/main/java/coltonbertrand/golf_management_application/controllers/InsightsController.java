6bc0285cfcac9fabdb8dab07ea7f1869a4f18506
colton <coltonbertrand14@gmail.com>
2025-02-18 12:33:13 -0400

    Modified the prompt

--- src/main/java/coltonbertrand/golf_management_application/controllers/InsightsController.java
+++ src/main/java/coltonbertrand/golf_management_application/controllers/InsightsController.java
@@ -31,7 +31,7 @@ public class InsightsController {
                         " For a 9 hole round this would be 3 and for a 18 hole round this would be 6"+
                         " When a user is doing a good job on hitting fairways say congrats, also if you notice improvement. Offer other golf tips and general advice too" +
                         " Also tell the user how much they can lower their score if they improve in these areas " +
-                        " Recommend courses near the user if they have a location, if not don't mention it" +
+                        " Recommend courses near the user if they have a location, if not don't mention it. Try not to mention private courses" +
                         " Each round is delimited by ```"+
                         " please keep the response below 75 words.")
                 .build();
@@ -47,13 +47,12 @@ public class InsightsController {
             StringBuilder prompt = new StringBuilder();
             for(Rounds round : topFiveRounds) {
                 prompt.append("```Users Name: %s, User Location: %s, Date Played: %s, Round Length: %d, Three Putts: %d, Slices or Draws: %d, Fairways Hit: %d```"
-                        .formatted(round.getUser().getFirstName(), userLocation, round.getDatePlayed(),round.getRoundLength(), round.getThreePutts(), round.getSlicesOrDraws(), round.getFairwaysHit()));
+                        .formatted(user.getFirstName(), userLocation, round.getDatePlayed(),round.getRoundLength(), round.getThreePutts(), round.getSlicesOrDraws(), round.getFairwaysHit()));
             }
             String response = chatClient.prompt()
                     .user(prompt.toString())
                     .call()
                     .content();
-            System.out.println(response);
             return ResponseEntity.ok().body(response);
     }
 }
